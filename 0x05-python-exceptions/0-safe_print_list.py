1648bffd30fe4250bc38cfd28b454c4ca0089525
kevin kirui <kipkoechkevin42@gmail.com>
2024-01-22 06:18:53 -0800

    created files

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        my_list = ['Kevin', True, 'Vinnie', False, 'Dee', 2405, 2.30]
-        print(my_list)
-    except ValueError:
-        print(f"List is empty: {[]}", my_list)
-safe_print_list(['Kevin'], 2)
+    num = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            num += 1
+        except IndexError:
+            break
+    print("")
+    Return
--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    print("{:d}".format(num))
-print_integer(42)    
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False    
