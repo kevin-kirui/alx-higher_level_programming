743e1b8a5042f990d72c70634e9df99ebc160591
kevin kirui <kipkoechkevin42@gmail.com>
2024-04-25 13:06:45 -0700

    post

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -9,16 +9,19 @@ fi
 # Define the URL from the argument
 url="$1"
 
-# Set the header and value
-header="X-School-User-Id: 98"
+# Set headers and their values
+email="test@gmail.com"
+subject="I will always be here for PLD"
+header1="X-Email: $email"
+header2="X-Subject: $subject"
 
-# Use curl to get the response body with the header
-response=$(curl -s -H "$header" "$url")
+# Use curl to send the POST request with headers
+response=$(curl -s -X POST -H "$header1" -H "$header2" "$url")
 
 # Check if curl command was successful (exit code 0)
 if [ $? -eq 0 ]; then
   echo "Response body:"
   echo "$response"
 else
-  echo "Error: curl failed to retrieve the response body."
+  echo "Error: curl failed to post data."
 fi
