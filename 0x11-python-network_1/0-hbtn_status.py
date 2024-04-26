8f737d2905984a7ca4ce37c432ca2f808a245031
kevin kirui <kipkoechkevin42@gmail.com>
2024-04-26 12:04:03 -0700

    status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,16 @@
-import urllib.request
+#!/usr/bin/python3
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
+"""
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
-    print("- type: {}".format(type(html)))
-    print("- content: {}".format(html))
-    print("- utf8 content: {}".format(html.decode('utf-8')))
 
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
